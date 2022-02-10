3c00a5e2d8cfa1f532b1907208c99c401ff88043
CleSucre <julien.thomas.4321@gmail.com>
2022-02-10 20:22:52 +0100

    Reformat
    Reformat: Reformatting ugly code

--- query.php
+++ query.php
@@ -19,12 +19,14 @@ function query(string $host, int $port, int $timeout = 4) : bool|array{
     $socket = @fsockopen('udp://'.$host, $port, $errno, $errstr, $timeout);
     if($errno) {
         fclose($socket);
-        if (DEBUG > 0)
+        if (DEBUG > 0) {
             echo $errstr . " " . $errno;
+        }
         return false;
     } else if($socket === false) {
-        if (DEBUG > 0)
+        if (DEBUG > 0) {
             echo $errstr . " " . $errno;
+        }
         return false;
     }
     stream_Set_Timeout($socket, $timeout);
@@ -35,25 +37,29 @@ function query(string $host, int $port, int $timeout = 4) : bool|array{
     $command .= \pack('Q', 2);
     $length = \strlen($command);
     if($length !== fwrite($socket, $command, $length)) {
-        if (DEBUG > 0)
+        if (DEBUG > 0) {
             echo "Failed to write on socket. " . E_WARNING;
+        }
         return false;
     }
     $data = fread($socket, 4096);
     fclose($socket);
     if(empty($data) or $data === false) {
-        if (DEBUG > 0)
+        if (DEBUG > 0) {
             echo "Server failed to respond " . E_WARNING;
+        }
         return false;
     }
     if(substr($data, 0, 1) !== "\x1C") {
-        if (DEBUG > 0)
+        if (DEBUG > 0) {
             echo "First byte is not ID_UNCONNECTED_PONG. " . E_WARNING;
+        }
         return false;
     }
     if(substr($data, 17, 16) !== $OFFLINE_MESSAGE_DATA_ID) {
-        if (DEBUG > 0)
+        if (DEBUG > 0) {
             echo "Magic bytes do not match.";
+        }
         return false;
     }
     $data = \substr($data, 35);
@@ -73,26 +79,29 @@ function query(string $host, int $port, int $timeout = 4) : bool|array{
     ];
 }
 
-$address = readline('Adresse: ');
+$address = readline('Adresse (127.0.0.1): ');
 if ($address == "") {
-    echo "Please enter a valid address";
-    exit(1);
+    $address = "127.0.0.1";
 }
+
 $port = readline('Port (19132): ');
-if ($port == "")
+if ($port == "") {
     $port = 19132;
+}
+
 $timeout = readline('Timeout (4): ');
-if ($timeout == "")
+if ($timeout == "") {
     $timeout = 4;
+}
 
 echo "Starting to query server " . $address . ":" . $port . " with a time out of " . $timeout . " secondes ...\n";
 $result = query($address, $port, $timeout);
 if (!$result) {
-    echo "No server found";
+    echo "No server found\n";
     exit(0);
 }
+
 foreach ($result as $key => $item) {
     echo $key . ": " . $item . "\n";
 }
 exit(0);
-?>
